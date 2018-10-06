6a51e4939ecb1760f946c216adf28aa71c59825b
Michael Cao <mike.x.cao@gmail.com>
2018-10-06 19:27:36 -0400

    working uploading

--- app.py
+++ app.py
@@ -3,76 +3,66 @@ import requests
 
 from flask import Flask, request, render_template, send_from_directory
 
-__author__ = 'mrmitty'
-
 app = Flask('__name__')
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 @app.route('/')
 def index():
     return render_template("upload.html")
-    #this is the function for calling the render template
+    # this is the function for calling the render template
 
 
-@app.route('/upload',methods=["POST"])
-#method for uploading the file
+@app.route('/upload', methods=["POST"])
+# method for uploading the file
 def upload():
-    folder_name =   "images"
-    
     # this is to verify that folder to upload to exists.
-    if os.path.isdir(os.path.join(APP_ROOT, 'files/{}'.format(folder_name))):
+    if os.path.isdir(os.path.join(APP_ROOT, 'files')):
         print("folder exist")
-    
 
-    target = os.path.join(APP_ROOT, 'files/{}'.format(folder_name))
+    target = os.path.join(APP_ROOT, 'files')
     print(target)
-    #print the file name
+    # print the file name
     print("-------------------------------------------")
-    #if (not os.path.isdir(target)):
-    os.mkdir(target)
+    if (not os.path.isdir(target)):
+        os.mkdir(target)
     print("good target")
     print("-------------------------------------------")
-        #if the directory exists, good. if not, then make it
+    # if the directory exists, good. if not, then make it
     print(request.files.getlist("files"))
     print("-------------------------------------------")
-    #print the list of files requested...i think
+    # print the list of files requested...i think
 
-    
     for upload in request.files.getlist("file"):
         print(upload)
         print('{} is the file name'.format(upload.filename))
         filename = upload.filename
-        #this is to verify that files are supported
+        # this is to verify that files are supported
         ext = os.path.splitext(filename)[1]
         if (ext == ".jpg") or (ext == ".png"):
             print("File is supported...moving on")
         else:
-            render_template("Error.html", message="Files were not supported: "+ext)
+            render_template(
+                "error.html", message="Files were not supported: "+ext)
         print("-------------------------------------------")
-        destination = target + "/" + filename    #adds the target directory path to the string filename with the / seperator
+        # adds the target directory path to the string filename with the / seperator
+        destination = target + "/" + filename
         print("-------------------------------------------")
         print("Accept incoming File: ", filename)
         print("Save it to: ", destination)
         print("-------------------------------------------")
         upload.save(destination)
-        with open(destination, 'rb') as f:
-            r = requests.post('http://i.image2haiku.com/upload.php', files={destination: f}, data={'secret':"image2haiku"})
-            print(r.content)
-            return r.content
-    
 
-    #return send_from_directory("images", filename, as_attachment=True)
-    return render_template("./complete.html",image_name=filename) #will show the file icon and tell that it is completed
+    # return send_from_directory("images", filename, as_attachment=True)
+    # will show the file icon and tell that it is completed
+    return "http://127.0.0.1:5000/files/" + filename
 
-@app.route('/gallery')
-#will display all images
-def get_gallery():
-    image_names = os.listdir('./images')
-    print(image_names)
-    return render_template("./gallery.html", image_names=image_names)
+
+@app.route('/files/<path:path>')
+def send_file(path):
+    return send_from_directory('files', path)
+
 
 if __name__ == "__main__":
-    app.run(port=4555, debug=True)
-
-    
+    app.run(port=5000, debug=True)
--- getConcepts_clarifai.py
+++ getConcepts_clarifai.py
@@ -3,9 +3,11 @@ from clarifai.rest import Image as ClImage
 
 app = ClarifaiApp(api_key='368e6551d02a46b495f9724f7d7d2683')
 
-def getClarifai(imgurl, CONFIDENCE_THRESHOLD = 0.8, MAX_CONCEPTS = 100):
+
+def getClarifai(imgurl, CONFIDENCE_THRESHOLD=0.8, MAX_CONCEPTS=100):
     model = app.models.get('general-v1.3')
-    prediction = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg', max_concepts=MAX_CONCEPTS, min_value=CONFIDENCE_THRESHOLD)
+    prediction = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg',
+                                      max_concepts=MAX_CONCEPTS, min_value=CONFIDENCE_THRESHOLD)
     print("finished prediction")
 
     # Get data out of prediction
@@ -14,5 +16,5 @@ def getClarifai(imgurl, CONFIDENCE_THRESHOLD = 0.8, MAX_CONCEPTS = 100):
     for concept in concepts:
         if concept["value"] >= CONFIDENCE_THRESHOLD:
             valid_concepts.append(concept["name"])
-    
+
     return valid_concepts
