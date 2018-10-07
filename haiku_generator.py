b831b2cc43e0f4e80ebf34d0dee4780e51c3891a
Sriparno Majumdar <szm6174@psu.edu>
2018-10-07 13:14:11 -0400

    fuck yeeeaaah

--- haiku_generator.py
+++ haiku_generator.py
@@ -1,15 +1,79 @@
-#phrase generator
 import random
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] =  "D:\Python\HACKPSU\\Natural-lang-1-ab3088af655f.json"
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+import six
+import sys
 
-adj_lst = [ ['happy', 'sad', 'jubilant'], [2,1,3]]
-noun_lst = [ ['dog', 'cat', 'sophia'], [1,2,3]]
-verb_lst = [ ['running', 'sleeping', 'superrcalifragil'], [2,1,7]]
 
-#noun_dict = {"dog":1, "cat":2, "sophia":3}
-#adj_dict  = {"happy":2, "sad":1, "jubilant":3}
-#verb_dict = {"running":2, "sleeping":1, "supercalifragil":7}
 
-def adj_lst_to_dict(adj_lst): #, noun_lst, verb_lst):
+def finding_nouns_and_shit(dicti):
+    client = language.LanguageServiceClient()
+    
+    doc = types.Document(content=dicti, type=enums.Document.Type.PLAIN_TEXT)
+    nouns_n_shit=client.analyze_syntax(doc).tokens
+    pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM',
+               'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
+    noun = []
+    verb = []
+    adj = []
+    for token in nouns_n_shit:
+        if (pos_tag[token.part_of_speech.tag]=='NOUN'):
+            noun.append(token.text.content)
+        if (pos_tag[token.part_of_speech.tag]=='VERB'):
+            verb.append(token.text.content)
+        if (pos_tag[token.part_of_speech.tag]=='ADJ'):
+            adj.append(token.text.content)
+   
+    return noun,verb,adj
+
+#https://stackoverflow.com/questions/46759492/syllable-count-in-python
+
+
+def syllable_count(some_list):
+    syllable_list = []
+
+    for i in range(len(some_list)):
+        some_list[i] = some_list[i].lower()
+
+        word   = some_list[i]
+        count  = 0
+        vowels = "aeiouy"
+
+        if word[0] in vowels:
+            count += 1
+
+        for index in range(1, len(word)):
+            if word[index] in vowels and word[index - 1] not in vowels:
+                count += 1
+                if word.endswith("e"):
+                    count -= 1
+
+        if count == 0:
+            count += 1
+
+        syllable_list.append(count)
+
+    return syllable_list
+
+def adj_to_adj_lst(adj):
+    adj_lst = [adj, syllable_count(adj)]
+    return adj_lst
+
+def noun_to_noun_lst(noun):
+    noun_lst = [noun, syllable_count(noun)]
+    return noun_lst
+
+def verb_to_verb_lst(verb):
+    verb_lst = [verb, syllable_count(verb)]
+    return verb_lst
+
+
+def adj_lst_to_dict(adj_lst):
     adj_dict = {}
     for i in range(len(adj_lst)+1):
         adj_dict[ adj_lst[0][i] ] = adj_lst[1][i]
@@ -97,6 +161,7 @@ def sec_line_generator(adj_dict, noun_dict, verb_dict):
             return second_line
 
         tries += 1
+        
 def third_line_generator(adj_dict, noun_dict, verb_dict):
     #third line
     syllable   = 0
@@ -131,10 +196,79 @@ def third_line_generator(adj_dict, noun_dict, verb_dict):
 
         tries += 1
 
-if __name__ == "__main__":
-    adj_dict = adj_lst_to_dict(adj_lst)
-    noun_dict = noun_lst_to_dict(noun_lst)
-    verb_dict = verb_lst_to_dict(verb_lst)
-    print(first_line_generator(adj_dict, noun_dict, verb_dict))
-    print(sec_line_generator(adj_dict, noun_dict, verb_dict))
-    print(third_line_generator(adj_dict, noun_dict, verb_dict))
+def calculate_sentiment_of_phrase(phrase_list):
+
+    client = language_v1.LanguageServiceClient()
+    phrase_sentiment_list = []
+
+    for i in range(len(phrase_list)):
+    
+        type_    = enums.Document.Type.PLAIN_TEXT
+        document = {'type': type_, 'content': phrase_list[i]}
+
+        resp = client.analyze_sentiment(document)
+        sentiment = resp.document_sentiment
+        phrase_sentiment_list.append(sentiment.score)
+    return phrase_sentiment_list
+
+
+def main_shit(word_list, final_senti):
+
+    dicti = dictionary(word_list)
+    
+    finding_nouns_n_shit(dicti)
+    
+    
+    adj_dict = adj_lst_to_dict(adj_to_adj_lst(adj)))
+    noun_dict = noun_lst_to_dict(noun_to_noun_lst(noun)))
+    verb_dict = verb_lst_to_dict(verb_to_verb_lst(verb)))
+    
+    first_line_list  = []
+    second_line_list = []
+    third_line_list  = []
+    for i in range(5):
+        first_line_list.append(first_line_generator(adj_dict, noun_dict, verb_dict))
+        second_line_list.append(sec_line_generator(adj_dict, noun_dict, verb_dict))
+        third_line_list.append(third_line_generator(adj_dict, noun_dict, verb_dict))
+
+    first_line_senti = calculate_sentiment_of_phrase(first_line_list)
+    second_line_senti = calculate_sentiment_of_phrase(second_line_list)
+    third_line_senti = calculate_sentiment_of_phrase(third_line_list)
+
+    final_senti = black_magic(res,emo) #must fix with mike
+    small = 0
+    small = final_senti - first_line_senti[0]
+    i=1
+    for i in range(5):
+        if(final_senti - first_line_senti[i] <= small):
+            small = final_senti - first_line_senti[i]
+    i = 0
+    for i in range(5):
+        if(small == final_senti - first_line_senti[i]):
+            break
+    small = 0
+    small = final_senti - second_line_senti[0]
+    j=1
+    for j in range(5):
+        if(final_senti - second_line_senti[j] <= small):
+            small = final_senti - second_line_senti[i]
+    j = 0
+    for j in range(5):
+        if(small == final_senti - second_line_senti[j]):
+            break
+    small = 0
+    small = final_senti - first_line_senti[0]
+    k=1
+    for k in range(5):
+        if(final_senti - third_line_senti[k] <= small):
+            small = final_senti - third_line_senti[i]
+    k = 0
+    for k in range(5):
+        if(small == final_senti - third_line_senti[k]):
+            break
+
+    first_line  = first_line_list[i] #change
+    second_line = second_line_list[j]
+    third _line = third_line_list[k]
+    
+    return first_line,second_line,third_line
