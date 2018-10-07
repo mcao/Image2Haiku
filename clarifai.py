946090649cb8bb78c3019c72e7e083d7e2ce0ab7
Michael Cao <mike.x.cao@gmail.com>
2018-10-06 23:11:02 -0400

    linting

--- clarifai.py
+++ clarifai.py
@@ -1,25 +1,26 @@
 from clarifai.rest import ClarifaiApp
-from clarifai.rest import Image as ClImage
-import colorsys
 
-app = ClarifaiApp(api_key='368e6551d02a46b495f9724f7d7d2683')
+API_KEY = "368e6551d02a46b495f9724f7d7d2683"
+APP = ClarifaiApp(api_key=API_KEY)
 
 
-def getConcepts(imgurl='https://samples.clarifai.com/metro-north.jpg', CONFIDENCE_THRESHOLD=0.8, MAX_CONCEPTS=100):
-    model = app.models.get('general-v1.3')
-    prediction = model.predict_by_url(url=imgurl, max_concepts=MAX_CONCEPTS, min_value=CONFIDENCE_THRESHOLD)
+def get_concepts(imgurl='https://samples.clarifai.com/metro-north.jpg', threshold=0.8, max_concepts=100):
+    model = APP.models.get('general-v1.3')
+    prediction = model.predict_by_url(url=imgurl,
+                                      max_concepts=max_concepts, min_value=threshold)
 
     # Get data out of prediction
     concepts = prediction["outputs"][0]["data"]["concepts"]
     valid_concepts = []
     for concept in concepts:
-        if concept["value"] >= CONFIDENCE_THRESHOLD:
+        if concept["value"] >= threshold:
             valid_concepts.append(concept["name"])
 
     return valid_concepts
 
-def getColors(imgurl='https://samples.clarifai.com/metro-north.jpg', PERCENT_THRESHOLD=0.1, MAX_CONCEPTS=100):
-    model = app.models.get('color')
+
+def get_colors(imgurl='https://samples.clarifai.com/metro-north.jpg', threshold=0.1):
+    model = APP.models.get('color')
     prediction = model.predict_by_url(url=imgurl)
 
     # Get data out of prediction
@@ -32,10 +33,11 @@ def getColors(imgurl='https://samples.clarifai.com/metro-north.jpg', PERCENT_THR
         #     b = int(color["w3c"]["hex"][5:], 16)/255
         #     hsv = colorsys.rgb_to_hsv(r,g,b)
         #     print(hsv)
-        if color["value"] >= PERCENT_THRESHOLD:
-            colorName = color["w3c"]["name"].lower()
-            colorNames = ["white","grey","gray","black","red","orange","yellow","green","blue","purple","violet"]
-            for c in colorNames:
-                if c in colorName and c not in valid_colors:
+        if color["value"] >= threshold:
+            color_name = color["w3c"]["name"].lower()
+            color_names = ["white", "grey", "gray", "black", "red",
+                           "orange", "yellow", "green", "blue", "purple", "violet"]
+            for c in color_name:
+                if c in color_names and c not in valid_colors:
                     valid_colors.append(c)
     return valid_colors
