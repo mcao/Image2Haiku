64d36c376a7ab94089c68c982329410949d50193
t-wang01 <t.wang01@yahoo.com>
2018-11-04 23:12:17 -0500

    Cleaned up code

--- clarifai_data.py
+++ clarifai_data.py
@@ -14,29 +14,23 @@ def get_concepts(imgurl, threshold=0.8, max_concepts=100):
 
     # Get data out of prediction
     concepts = prediction["outputs"][0]["data"]["concepts"]
-    valid_concepts = []
-    for concept in concepts:
-        if concept["value"] >= threshold:
-            valid_concepts.append(concept["name"])
-
-    return valid_concepts
-
+    return [concept["name"] for concept in concepts]
 
 def get_colors(imgurl, threshold=0.1):
     assert isinstance(imgurl, str), "Invalid image input"
 
     # Set up clarifai api
     model = APP.models.get('color')
-    prediction = model.predict_by_url(url=imgurl, min_value=threshold)
+    prediction = model.predict_by_url(url=imgurl)
 
     # Get data out of prediction
-    colors = prediction["outputs"][0]["data"]["colors"]
+    color_data = prediction["outputs"][0]["data"]["colors"]
+    colors = [color["w3c"]["name"].lower() for color in color_data if color["value"] >= threshold]
     color_names = ["white", "grey", "gray", "black", "red",
                    "orange", "yellow", "green", "blue", "purple", "violet"]
     valid_colors = []
     for color in colors:
-        color_name = color["w3c"]["name"].lower()
         for c in color_names:
-            if c in color_name and c not in valid_colors:
+            if c in color and c not in valid_colors:
                 valid_colors.append(c)
     return valid_colors
